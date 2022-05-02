8fbb74e6f4954c3fe4b57f16cc9c13f042434e26
Jaeyeop-Jung <jjy5349@naver.com>
2022-05-02 12:35:26 +0900

    feature: AssetTest 테스트 코드 작성

--- chaincode-AssetTransfer/src/test/java/org/hyperledger/fabric/samples/assettransfer/AssetTest.java
+++ chaincode-AssetTransfer/src/test/java/org/hyperledger/fabric/samples/assettransfer/AssetTest.java
@@ -1,74 +1,37 @@
-///*
-// * SPDX-License-Identifier: Apache-2.0
-// */
-//
-//package org.hyperledger.fabric.samples.assettransfer;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//import org.junit.jupiter.api.Nested;
-//import org.junit.jupiter.api.Test;
-//
-//public final class AssetTest {
-//
-//    @Nested
-//    class Equality {
-//
-//        @Test
-//        public void isReflexive() {
-//            Asset asset = new Asset("asset1", "Blue", 20, "Guy", 100);
-//
-//            assertThat(asset).isEqualTo(asset);
-//        }
-//
-//        @Test
-//        public void isSymmetric() {
-//            Asset assetA = new Asset("asset1", "Blue", 20, "Guy", 100);
-//            Asset assetB = new Asset("asset1", "Blue", 20, "Guy", 100);
-//
-//            assertThat(assetA).isEqualTo(assetB);
-//            assertThat(assetB).isEqualTo(assetA);
-//        }
-//
-//        @Test
-//        public void isTransitive() {
-//            Asset assetA = new Asset("asset1", "Blue", 20, "Guy", 100);
-//            Asset assetB = new Asset("asset1", "Blue", 20, "Guy", 100);
-//            Asset assetC = new Asset("asset1", "Blue", 20, "Guy", 100);
-//
-//            assertThat(assetA).isEqualTo(assetB);
-//            assertThat(assetB).isEqualTo(assetC);
-//            assertThat(assetA).isEqualTo(assetC);
-//        }
-//
-//        @Test
-//        public void handlesInequality() {
-//            Asset assetA = new Asset("asset1", "Blue", 20, "Guy", 100);
-//            Asset assetB = new Asset("asset2", "Red", 40, "Lady", 200);
-//
-//            assertThat(assetA).isNotEqualTo(assetB);
-//        }
-//
-//        @Test
-//        public void handlesOtherObjects() {
-//            Asset assetA = new Asset("asset1", "Blue", 20, "Guy", 100);
-//            String assetB = "not a asset";
-//
-//            assertThat(assetA).isNotEqualTo(assetB);
-//        }
-//
-//        @Test
-//        public void handlesNull() {
-//            Asset asset = new Asset("asset1", "Blue", 20, "Guy", 100);
-//
-//            assertThat(asset).isNotEqualTo(null);
-//        }
-//    }
-//
-//    @Test
-//    public void toStringIdentifiesAsset() {
-//        Asset asset = new Asset("asset1", "Blue", 20, "Guy", 100);
-//
-//        assertThat(asset.toString()).isEqualTo("Asset@e04f6c53 [assetID=asset1, color=Blue, size=20, owner=Guy, appraisedValue=100]");
-//    }
-//}
+package org.hyperledger.fabric.samples.assettransfer;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class AssetTest {
+
+    @Test
+    public void 같은_Asset_을_테스트한다(){
+        HashMap<String, String> coin = new HashMap<>();
+        Asset asset = Asset.of("asset1", "test", coin, null, null, null);
+
+        assertThat(asset).isEqualTo(asset);
+    }
+
+    @Test
+    public void 같은_값을_가진_Asset_을_테스트한다(){
+        HashMap<String, String> coin = new HashMap<>();
+        Asset asset1 = Asset.of("asset1", "test", coin, null, null, null);
+        Asset asset2 = Asset.of("asset1", "test", coin, null, null, null);
+
+        assertThat(asset1).isEqualTo(asset2);
+    }
+
+    @Test
+    public void 다른_Assets_을_테스트한다(){
+        HashMap<String, String> coin = new HashMap<>();
+        Asset asset1 = Asset.of("asset1", "test1", coin, null, null, null);
+        Asset asset2 = Asset.of("asset2", "test2", coin, null, null, null);
+
+        assertThat(asset1).isNotEqualTo(asset2);
+    }
+}
